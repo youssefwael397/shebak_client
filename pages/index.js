02e7bd000625ef106207050f915fcace671a6f0f
youssefwael <youssefwael397>
2022-11-19 20:56:53 +0200

    fix errors

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
 export default function Home() {
+
   return (
-      <h1>Hello world!</h1>
+    <>
+
+    </>
   )
 }
+
--- store/slices/authSlice.js
+++ store/slices/authSlice.js
@@ -1,26 +1,26 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-export const signUp = createAsyncThunk('auth/signup', async (args, thunkApi) => {
-try {
-    const url = '/api/signup'
-    // const res = axios;
-} catch (error) {
+// export const signUp = createAsyncThunk('auth/signup', async (args, thunkApi) => {
+// try {
+//     const url = '/api/signup'
+//     // const res = axios;
+// } catch (error) {
     
-}
-})
+// }
+// })
 
 const initialState = {
     is_loading: false,
     api_errors: [],
     is_success: false,
-
 }
 
 export const authSlice = createSlice({
-    name: '',
+    name: 'auth',
     initialState,
     reducers: {},
     // extraReducers: 
 })
 
 
+export default authSlice.reducer
