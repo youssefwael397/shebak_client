bb62f2d036b1cea09e465da6eefacbbdbd38ba9a
youssefwael <youssefwael397>
2022-11-25 19:30:30 +0200

    create async thunk

---
+++ components/Messages.js
@@ -0,0 +1,16 @@
+import { Button, notification, Space } from 'antd';
+import React from 'react';
+
+export const NotifyMessage = ({ type, title, description }) => {
+    notification[type]({
+        message: title,
+        description: typeof description === 'object' ?
+            <>{
+                description.map(elem => <>
+                    <span className='d-block'>{elem}</span>
+                    <br />
+                </>)
+            }</>
+            : description
+    });
+};
