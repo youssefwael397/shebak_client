ba3813098286327d92005078da96b3da266e83f7
youssefwael <youssefwael397>
2022-12-13 14:54:54 +0200

    setup socket.io-client

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
 import styles from "../styles/Stream.module.css";
 import StreamVideo from "../components/StreamVideo";
+import io from 'socket.io-client';
+import { useEffect, useState } from "react";
+
 
 const Stream = () => {
+  const [socket, setSocket] = useState(null);
+  const [message, setMessage] = useState("message");
+
+  useEffect(() => {
+    const newSocket = io(`http://localhost:5000`);
+    console.log(newSocket);
+    setSocket(newSocket);
+    // return () => newSocket.close();
+  }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message) {
+      console.log(socket)
+      socket.emit('message', { "message": message });
+    }
+  };
+
   return (
     <div className={`${styles.stream} py-5`}>
       <div className="container">
@@ -11,6 +32,23 @@ const Stream = () => {
           </div>
         </div>
       </div>
+
+      {socket ? (
+        <div className="chat-container mt-3">
+          <form>
+            <input
+              value={message}
+              placeholder="Type your message"
+              onChange={(e) => {
+                setMessage(e.target.value)
+              }}
+            />
+            <button type="submit" onClick={sendMessage}>Send</button>
+          </form>
+        </div>
+      ) : (
+        <div>Socket Not Connected</div>
+      )}
     </div>
   );
 };
