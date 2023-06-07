1e871fa285a49d97c6b7a45ceb0edee73ccdacea
youssefwael <youssefwael397>
2023-06-07 23:42:07 +0300

    refactor bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
-import "../styles/globals.css";
-import Head from "next/head";
-import Script from "next/script";
-import Layout from "../components/Layout";
-import { Provider } from "react-redux";
-import { store } from "../store/store";
-import "antd/dist/antd.css";
-import { ThemeProvider } from 'next-themes'
+import '../styles/globals.css';
+import Head from 'next/head';
+import Script from 'next/script';
+import Layout from '../components/Layout';
+import { Provider } from 'react-redux';
+import { store } from '../store/store';
+import 'antd/dist/antd.css';
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -17,26 +17,18 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="images/logo5.png" />
       </Head>
 
-
-
-      {/* <Script 
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" 
-      integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" 
-      crossOrigin="anonymous" 
-      /> */}
-
       <Script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js" />
 
       <Provider store={store}>
-        {
-          Component.getLayout ?
-            Component.getLayout(<Component {...pageProps} />) :
-            <Layout>
-              <ThemeProvider>
-                <Component {...pageProps} />
-              </ThemeProvider>
-            </Layout>
-        }
+        {Component.getLayout ? (
+          Component.getLayout(<Component {...pageProps} />)
+        ) : (
+          <Layout>
+            <ThemeProvider>
+              <Component {...pageProps} />
+            </ThemeProvider>
+          </Layout>
+        )}
       </Provider>
     </>
   );
--- store/slices/userSlice.js
+++ store/slices/userSlice.js
@@ -1,90 +1,86 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { NotifyMessage } from "../../components/Messages";
-import axiosInstance from "../../lib/axiosInstancs"
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { NotifyMessage } from '../../components/Messages';
+import axiosInstance from '../../lib/axiosInstancs';
 
 export const getUsers = createAsyncThunk(
-    "users",
-    async (args, { rejectWithValue }) => {
-        try {
-            console.log('register thunk')
-            const url = "/api/users";
-            const res = await axiosInstance.get(url);
-            return res.data;
-        } catch (err) {
-            return rejectWithValue(err);
-        }
+  'users',
+  async (args, { rejectWithValue }) => {
+    try {
+      const url = '/api/users';
+      const res = await axiosInstance.get(url);
+      return res.data;
+    } catch (err) {
+      return rejectWithValue(err);
     }
+  }
 );
 
-
 export const getUser = createAsyncThunk(
-    "user",
-    async (id, { rejectWithValue }) => {
-        try {
-            console.log('register thunk')
-            const url = `/api/users/${id}`;
-            const res = await axiosInstance.get(url);
-            return res.data;
-        } catch (err) {
-            return rejectWithValue(err);
-        }
+  'user',
+  async (id, { rejectWithValue }) => {
+    try {
+      const url = `/api/users/${id}`;
+      const res = await axiosInstance.get(url);
+      return res.data;
+    } catch (err) {
+      return rejectWithValue(err);
     }
+  }
 );
 
 const initialState = {
-    is_loading: false,
-    is_success: false,
-    api_errors: null,
-    users: [],
-    user_warning: null,
-    imgPath: "http://localhost:8000"
-}
+  is_loading: false,
+  is_success: false,
+  api_errors: null,
+  users: [],
+  user_warning: null,
+  imgPath: 'http://localhost:8000',
+};
 
 export const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-        ResetSuccess(state) {
-            state.success = null;
-        },
+  name: 'user',
+  initialState,
+  reducers: {
+    ResetSuccess(state) {
+      state.success = null;
     },
-    extraReducers: {
-        // get users
-        [getUsers.pending]: (state, { payload }) => {
-            state.is_loading = true;
-        },
-        [getUsers.fulfilled]: (state, { payload }) => {
-            state.is_loading = false;
-            state.is_success = true;
-            state.api_errors = null;
-            state.users = payload.users;
-        },
-        [getUsers.rejected]: (state, { payload }) => {
-            state.is_loading = false;
-            state.is_success = false;
-        },
-        // get user
-        [getUser.pending]: (state, { payload }) => {
-            state.is_loading = true;
-            state.user_warning = null;
-        },
-        [getUser.fulfilled]: (state, { payload }) => {
-            state.is_loading = false;
-            state.is_success = true;
-            state.api_errors = null;
-            state.user_warning = payload;
-        },
-        [getUser.rejected]: (state, { payload }) => {
-            state.is_loading = false;
-            state.is_success = false;
-            state.user_warning = null;
-            console.log(first)
-            state.api_errors = payload.response.data.message
-        }
-    }
-})
+  },
+  extraReducers: {
+    // get users
+    [getUsers.pending]: (state, { payload }) => {
+      state.is_loading = true;
+    },
+    [getUsers.fulfilled]: (state, { payload }) => {
+      state.is_loading = false;
+      state.is_success = true;
+      state.api_errors = null;
+      state.users = payload?.users;
+    },
+    [getUsers.rejected]: (state, { payload }) => {
+      state.is_loading = false;
+      state.is_success = false;
+    },
+    // get user
+    [getUser.pending]: (state, { payload }) => {
+      state.is_loading = true;
+      state.user_warning = null;
+    },
+    [getUser.fulfilled]: (state, { payload }) => {
+      state.is_loading = false;
+      state.is_success = true;
+      state.api_errors = null;
+      state.user_warning = payload;
+    },
+    [getUser.rejected]: (state, { payload }) => {
+      state.is_loading = false;
+      state.is_success = false;
+      state.user_warning = null;
+      state.api_errors = payload?.response?.data?.message;
+    },
+  },
+});
 
 // Action creators are generated for each case reducer function
 export const { ResetSuccess } = userSlice.actions;
 
-export default userSlice.reducer
+export default userSlice.reducer;
