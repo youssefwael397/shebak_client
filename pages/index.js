94b306f0cb5e4cd711e8464fbd3cddb35845e8cd
youssefwael <youssefwael397>
2023-06-08 04:17:44 +0300

    finish signUp

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,47 @@
-import styles from "../styles/Stream.module.css";
-import StreamVideo from "../components/StreamVideo";
+import styles from '../styles/Stream.module.css';
+import StreamVideo from '../components/StreamVideo';
 import io from 'socket.io-client';
-import { useEffect, useRef, useState } from "react";
-import { PlayCircleOutlined } from "@ant-design/icons";
-import CamStyles from "../styles/RecordVideo.module.css";
-import captureVideoFrame from "capture-video-frame";
-import HeaderTop from "../components/HeaderTop";
-import { useDispatch, useSelector } from "react-redux";
-import { startStream, stopStream } from "../store/slices/streamSlice";
+import { useEffect, useRef, useState } from 'react';
+import { PlayCircleOutlined } from '@ant-design/icons';
+import CamStyles from '../styles/RecordVideo.module.css';
+import captureVideoFrame from 'capture-video-frame';
+import HeaderTop from '../components/HeaderTop';
+import { useDispatch, useSelector } from 'react-redux';
+import { startStream, stopStream } from '../store/slices/streamSlice';
 import { Switch } from 'antd';
 
 const Stream = () => {
-  const { is_loading, is_streaming } = useSelector(state => state.stream)
+  const { is_loading, is_streaming } = useSelector((state) => state.stream);
   const [socket, setSocket] = useState(true);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [streamUrl, setStreamUrl] = useState('');
   const [open, setOpen] = useState(true);
 
-
   const handleStartStream = () => {
-    dispatch(startStream())
-  }
-
+    dispatch(startStream());
+    setOpen(true);
+  };
 
   const handleStopStream = () => {
-    dispatch(stopStream())
-  }
+    dispatch(stopStream());
+    setOpen(false);
+  };
+
+  const toggleStream = () => {
+    if (open == 1) {
+      handleStopStream();
+    } else {
+      handleStartStream();
+    }
+  };
 
   const onChange = (checked) => {
     console.log(`switch to ${checked}`);
   };
 
-  useEffect(() => { console.log(is_loading) }, [is_loading])
+  useEffect(() => {
+    console.log(is_loading);
+  }, [is_loading]);
 
   useEffect(() => {
     // Start the stream when the component mounts
@@ -43,7 +53,6 @@ const Stream = () => {
     };
   }, []);
 
-
   return (
     <>
       <div className={`${styles.stream} px-5`}>
@@ -53,30 +62,31 @@ const Stream = () => {
         </div>
         <div className={` pt-3 pb-3`}>
           <div className="container">
-            <div className={` mb-2 ${styles.cont_video} d-flex align-items-center justify-content-center`}>
+            <div
+              className={` mb-2 ${styles.cont_video} d-flex align-items-center justify-content-center`}
+            >
               <div className="d-flex align-items-center">
                 <div className="text-center mx-auto w-100 ms-5">
                   <div className="mb-5 ms-5">
-                    <div className={`ms-5 mt-3 ${CamStyles.videoContainer} ${CamStyles.StreamvideoContainer} position-relative`}>
-
+                    <div
+                      className={`ms-5 mt-3 ${CamStyles.videoContainer} ${CamStyles.StreamvideoContainer} position-relative`}
+                    >
                       {/* <div>Socket Connected</div> */}
 
-                      {
-                        is_loading ?
-                          <div className={`${styles.wstream} d-flex justify-content-center align-items-center rounded-4 border border-dark `}>
-                            <img
-                              src='../images/loading.gif'
-                              width="50"
-                            />
-                          </div>
-                          :
-                          <img
-                            src='http://localhost:8000/stream'
-                            width="650"
-                            height="455.5"
-                            className="rounded-4 border border-dark"
-                          />
-                      }
+                      {is_loading || !open ? (
+                        <div
+                          className={`${styles.wstream} d-flex justify-content-center align-items-center rounded-4 border border-dark `}
+                        >
+                          <img src="../images/loading.gif" width="50" />
+                        </div>
+                      ) : (
+                        <img
+                          src="http://localhost:8000/stream"
+                          width="650"
+                          height="455.5"
+                          className="rounded-4 border border-dark"
+                        />
+                      )}
 
                       {/* <div className="text-center mt-3"> */}
 
@@ -94,13 +104,25 @@ const Stream = () => {
                   </div>
                 </div>
                 <div className="text-white fs-6 ms-5 position-relative">
-                  <img className={styles.arrowsAnimatetop} src="../images/arrowsAnimate.gif" width="80px" />
+                  <img
+                    className={styles.arrowsAnimatetop}
+                    src="../images/arrowsAnimate.gif"
+                    width="80px"
+                  />
                   <div className="ms-5">
-                    <p className={`me-3 ms-2 ${!open ? "" : "text-white-50"}`}>Stop</p>
-                    <Switch onChange={setOpen} className={styles.switch} />
-                    <p className={`ms-2 mt-3 ${open ? "" : "text-white-50"}`}>Play</p>
+                    <p className={`me-3 ms-2 ${!open ? '' : 'text-white-50'}`}>
+                      Stop
+                    </p>
+                    <Switch onChange={toggleStream} className={styles.switch} />
+                    <p className={`ms-2 mt-3 ${open ? '' : 'text-white-50'}`}>
+                      Play
+                    </p>
                   </div>
-                  <img className={styles.arrowsAnimatebottom} src="../images/arrowsAnimate.gif" width="80px" />
+                  <img
+                    className={styles.arrowsAnimatebottom}
+                    src="../images/arrowsAnimate.gif"
+                    width="80px"
+                  />
                 </div>
               </div>
             </div>
--- pages/signUp.js
+++ pages/signUp.js
@@ -1,60 +1,208 @@
-import styles from "../styles/signUp.module.css";
-import { ShebakInput, ShebakInputIcon, ShebakLabel } from "../components/Inputs";
-import logo from "../public/images/suezuni.png";
-import Image from "next/image";
-import FaceRecorder from "../components/FaceRecorder";
-import { useRouter } from "next/router";
-import { useDispatch, useSelector } from "react-redux";
-import { registerSchema } from "../lib/validation/en/registerSchema";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { useEffect } from "react";
-import { register as signUp, ResetSuccess } from '../store/slices/authSlice'
-import RecorderComponent from "../components/Recorder";
+import styles from '../styles/signUp.module.css';
+import {
+  ShebakInput,
+  ShebakInputIcon,
+  ShebakLabel,
+} from '../components/Inputs';
+import logo from '../public/images/suezuni.png';
+import Image from 'next/image';
+import FaceRecorder from '../components/FaceRecorder';
+import { useRouter } from 'next/router';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  registerSchemaStepOne,
+  registerSchemaStepTwo,
+} from '../lib/validation/en/registerSchema';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useEffect } from 'react';
+import { register as signUp, ResetSuccess } from '../store/slices/authSlice';
+import RecorderComponent from '../components/Recorder';
 import { UserOutlined, MailOutlined } from '@ant-design/icons';
 import { Button } from 'antd';
 import { useState } from 'react';
 
-const SignUp = () => {
-  const router = useRouter()
-  const dispatch = useDispatch();
+const initialState = {
+  username: '',
+  email: '',
+  face_video: '',
+};
 
-  const { is_success, is_loading, registerInfo } = useSelector(
-    (state) => state.auth
+const FormStepOne = ({ setStep, formInfo, setFormInfo }) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    control,
+    setValue,
+    trigger,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(registerSchemaStepOne),
+    defaultValues: formInfo,
+    mode: 'all',
+  });
+
+  const onSubmit = (data) => {
+    setFormInfo({ ...formInfo, ...data });
+    setStep(2);
+  };
+
+  return (
+    <form
+      className={`w-75 position-absolute top-50 start-50 translate-middle`}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <>
+        <img
+          src="../images/logoSignUp.png"
+          width="190px"
+          className="mx-auto d-flex justify-content-center"
+        />
+        {/* <h3 className="fw-bold text-center text-dark mt-4 mb-3">Welcome to Shebak!</h3> */}
+        <h3 className="fw-bold text-center text-dark mt-4 mb-3">
+          Smart Security System
+        </h3>
+        <div className="row d-flex justify-content-center">
+          <div className="col-xxl-9 col-xl-9 col-lg-9 col-md-12 col-sm-12 mt-4">
+            <ShebakInputIcon
+              // register={register}
+              name="username"
+              control={control}
+              placeholder="Name"
+              icon={<UserOutlined />}
+              required
+            />
+            {errors.username?.message && (
+              <p className="m-0 text-danger">{errors.username?.message}</p>
+            )}
+          </div>
+          <div className="col-xxl-9 col-xl-9 col-lg-9 col-md-12 col-sm-12 mt-4">
+            <ShebakInputIcon
+              // register={register}
+              name="email"
+              control={control}
+              placeholder="Office Email"
+              icon={<MailOutlined />}
+              required
+            />
+            {errors.email?.message && (
+              <p className="m-0 text-danger">{errors.email?.message}</p>
+            )}
+          </div>
+        </div>
+
+        <div className={`d-flex justify-content-center mt-4`}>
+          <Button
+            // onClick={() => setStep(2)}
+            className={`${styles.btn} border-0 rounded-4 px-5 fs-6`}
+            type="primary"
+            htmlType="submit"
+          >
+            Next
+          </Button>
+        </div>
+      </>
+    </form>
   );
+};
+
+const FormStepTwo = ({ setStep, formInfo, setFormInfo }) => {
+  const { is_loading } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
   const {
     register,
     handleSubmit,
     watch,
     reset,
+    control,
     setValue,
+    trigger,
     formState: { errors },
   } = useForm({
-    resolver: yupResolver(registerSchema),
-    defaultValues: registerInfo,
+    resolver: yupResolver(registerSchemaStepTwo),
+    defaultValues: formInfo,
+    mode: 'all',
   });
 
-  const inputs = watch()
-
-  // useEffect(() => {
-  //   console.log("errors : ", errors)
-  //   console.log("inputs : ", inputs)
-  // }, [inputs, errors])
-
-  useEffect(() => {
-    if (is_success) {
-      reset()
-      dispatch(ResetSuccess())
-    }
-  }, [dispatch, is_success, reset]);
-
   const onSubmit = (data) => {
+    setFormInfo({ ...formInfo, ...data });
     dispatch(signUp(data));
   };
 
-  const [step, setStep] = useState(1);
+  return (
+    <form
+      className={`w-75 position-absolute top-50 start-50 translate-middle`}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <>
+        <ShebakLabel label="Recording Video" />
+        <div>
+          <FaceRecorder setValue={setValue} />
 
+          {errors.face_video?.message && (
+            <div className="text-danger text-center mt-4">
+              {errors.face_video.message}
+            </div>
+          )}
+        </div>
+
+        <div
+          className={`d-flex justify-content-center mt-4 ${
+            errors.face_video?.message ? 'mt-3' : 'mt-5'
+          }`}
+        >
+          <Button
+            onClick={() => setStep(1)}
+            className={`${styles.btn} me-4 text-white border-0 rounded-4 px-5 fs-6`}
+            htmlType="button"
+          >
+            Previous
+          </Button>
+
+          <Button
+            className={`border-0 rounded-4 px-5 fs-6 ${styles.disabled}`}
+            htmlType="submit"
+            // disabled
+          >
+            {is_loading ? 'Loading...' : 'Sign Up'}
+          </Button>
+
+          {/* <Button
+              className={`${styles.btn} border-0 rounded-4 px-5 fs-6`}
+              type="primary"
+              htmlType="submit"
+            >
+              {is_loading ? 'Loading...' : 'Sign Up'}
+            </Button> */}
+        </div>
+      </>
+    </form>
+  );
+};
+
+const SignUp = () => {
+  const dispatch = useDispatch();
+  const [step, setStep] = useState(1);
+  const [formInfo, setFormInfo] = useState(initialState);
+  const { is_success, is_loading, registerInfo } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    console.log(formInfo);
+  }, [formInfo]);
+
+  useEffect(() => {
+    if (is_success) {
+      console.log('succceesssssss');
+      setFormInfo(initialState);
+      setStep(1);
+      dispatch(ResetSuccess());
+    }
+  }, [dispatch, is_success, setFormInfo]);
 
   return (
     <div className={` `}>
@@ -64,110 +212,53 @@ const SignUp = () => {
           {/* <div className="container-xxl"> */}
           <div className="row">
             <div className={`col-xl-6 col-lg-6 ${styles.d_none}`}>
-
-              <lottie-player src="https://lottie.host/7b4e4ddf-a8d1-4742-b0d2-ea8eb2fdb801/XpSpqOPdac.json" background="transparent" speed="1" style={{ width: "450px", marginLeft: "auto", marginRight: "auto" }} loop autoplay></lottie-player>
-
+              <lottie-player
+                src="https://lottie.host/7b4e4ddf-a8d1-4742-b0d2-ea8eb2fdb801/XpSpqOPdac.json"
+                background="transparent"
+                speed="1"
+                style={{
+                  width: '450px',
+                  marginLeft: 'auto',
+                  marginRight: 'auto',
+                }}
+                loop
+                autoplay
+              ></lottie-player>
             </div>
-            <div className={`col-xl-6 col-lg-6 col-md-12 col-sm-12 ${styles.cont_right_section}`}>
+            <div
+              className={`col-xl-6 col-lg-6 col-md-12 col-sm-12 ${styles.cont_right_section}`}
+            >
               <div className={`${styles.right_section} position-relative`}>
-
                 <div className="d-flex justify-content-center">
                   <div className="">
-                    <img src="../images/arrow_left.png"
+                    <img
+                      src="../images/arrow_left.png"
                       width="200px"
                       className={` ${styles.arrowleft}`}
                     />
-                    <form className={`w-75 position-absolute top-50 start-50 translate-middle`} onSubmit={handleSubmit(onSubmit)}>
-                      {
-                        step == 1 ? <>
-                          <img
-                            src="../images/logoSignUp.png"
-                            width="190px"
-                            className="mx-auto d-flex justify-content-center"
-                          />
-                          {/* <h3 className="fw-bold text-center text-dark mt-4 mb-3">Welcome to Shebak!</h3> */}
-                          <h3 className="fw-bold text-center text-dark mt-4 mb-3">Smart Security System</h3>
-                          <div className="row d-flex justify-content-center">
-                            <div className="col-xxl-9 col-xl-9 col-lg-9 col-md-12 col-sm-12 mt-4">
-                              <ShebakInputIcon
-                                // register={register}
-                                // name="username"
-                                placeholder="Name"
-                                icon={<UserOutlined />}
-                              // required
-                              // error={errors.username?.message}
-                              />
-                            </div>
-                            <div className="col-xxl-9 col-xl-9 col-lg-9 col-md-12 col-sm-12 mt-4">
-                              <ShebakInputIcon
-                                // register={register}
-                                // name="email"
-                                placeholder="Office Email"
-                                icon={<MailOutlined />}
-                              // required
-                              // error={errors.email?.message}
-                              />
-                            </div>
-                          </div>
 
-                          <div className={`d-flex justify-content-center mt-4`}>
-                            <Button
-                              onClick={setStep}
-                              className={`${styles.btn} border-0 rounded-4 px-5 fs-6`}
-                              type="primary"
-                            // htmlType="submit"
-                            >
-                              Next
-                            </Button>
-                          </div>
-                        </> : <>
-                          <ShebakLabel label="Recording Video" />
-                          <div>
-                            <FaceRecorder setValue={setValue} />
+                    {step == 1 && (
+                      <FormStepOne
+                        setStep={setStep}
+                        formInfo={formInfo}
+                        setFormInfo={setFormInfo}
+                      />
+                    )}
+                    {step == 2 && (
+                      <FormStepTwo
+                        setStep={setStep}
+                        formInfo={formInfo}
+                        setFormInfo={setFormInfo}
+                      />
+                    )}
 
-                            {
-                              errors.face_video?.message &&
-                              <div className="text-danger mt-4">{errors.face_video.message}</div>
-                            }
-                          </div>
-
-                          <div className={`d-flex justify-content-center mt-4 ${errors.face_video?.message ? "mt-3" : "mt-5"}`}>
-
-                            <Button
-                              className={`${styles.btn} me-4 text-white border-0 rounded-4 px-5 fs-6`}
-                              onClick={setStep}
-                            >
-                              Previous
-                            </Button>
-
-                            <Button
-                              className={`border-0 rounded-4 px-5 fs-6 ${styles.disabled}`}
-                              disabled
-                            >
-                              Sign Up
-                            </Button>
-
-                            {/* <Button
-                              className={`${styles.btn} border-0 rounded-4 px-5 fs-6`}
-                              type="primary"
-                              htmlType="submit"
-                            >
-                              {is_loading ? "Loading..." : "Sign Up"}
-                            </Button> */}
-                          </div>
-                        </>
-                      }
-
-
-                    </form>
-                    <img src="../images/arrow_right.png"
+                    <img
+                      src="../images/arrow_right.png"
                       width="200px"
                       className={` ${styles.arrowright}`}
                     />
                   </div>
                 </div>
-
-
 
                 <div className={`position-absolute bottom-0 ${styles.dots}`}>
                   <img src="../images/dotsWhite.png" width="40px" />
@@ -177,21 +268,17 @@ const SignUp = () => {
                   <img src="../images/dotsGray.png" width="40px" />
                   <img src="../images/dotsGray.png" width="40px" />
                 </div>
-
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div >
+    </div>
   );
 };
 
-
-export default SignUp
+export default SignUp;
 
 SignUp.getLayout = function getLayout(page) {
-  return <>
-    {page}
-  </>
-}
+  return <>{page}</>;
+};
